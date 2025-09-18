1ecd73cb96192f5471c2ed5d56136ea0e13b4fd9
talentmapsolutions <talentmapsolutions2@gmail.com>
2025-09-18 10:44:48 +0530

    Update app.py
    adding two routes - /online , /www.talentmapsolutions

--- app.py
+++ app.py
@@ -7,7 +7,18 @@ def home():
     # Render your main workshop HTML page located in the templates folder
     return render_template('index.html')
 
+@app.route('/online')
+def home2():
+    # Render your main workshop HTML page located in the templates folder
+    return render_template('online.html') 
+
+@app.route('/www.talentmap.in')
+def website():
+    # Render your main workshop HTML page located in the templates folder
+    return render_template('www.talentmap.in')    
+
 if __name__ == '__main__':
     app.run(debug=False)
+
 
 
